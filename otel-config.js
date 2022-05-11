af8693135f05f9d33f2992c57e3b0cd92994a591
Jean-Louis Voiseux <48380853+jlvoiseux@users.noreply.github.com>
2022-05-11 18:37:16 +0200

    Add switch between grpc and http exporters

--- otel-config.js
+++ otel-config.js
@@ -1,27 +1,47 @@
 const api = require('@opentelemetry/api');
 const opentelemetry = require("@opentelemetry/sdk-node");
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
+const { OTLPTraceExporterGRPC } = require('@opentelemetry/exporter-trace-otlp-grpc');
+const { OTLPTraceExporterHTTP } = require('@opentelemetry/exporter-trace-otlp-http');
 const { Resource } = require('@opentelemetry/resources');
 const { SemanticResourceAttributes: ResourceAttributesSC } = require('@opentelemetry/semantic-conventions');
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 const { HostMetrics } = require('@opentelemetry/host-metrics');
 const apiMetrics = require('@opentelemetry/api-metrics');
 const { MeterProvider, ConsoleMetricExporter } = require('@opentelemetry/sdk-metrics-base');
-const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-grpc');
+const { OTLPMetricExporterGRPC } = require('@opentelemetry/exporter-metrics-otlp-grpc');
+const { OTLPMetricExporterHTTP } = require('@opentelemetry/exporter-metrics-otlp-http');
 const execSync = require('child_process').execSync;
 
 module.exports =  (serviceName, environment) => {
 
-  const traceExporter = new OTLPTraceExporter({
-    url: "http://apm-server:8200", // url is optional and can be omitted - default is http://localhost:55681/v1/traces
-    headers: {}, // an optional object containing custom headers to be sent with each request
-    concurrencyLimit: 1000, // an optional limit on pending requests
-  });
+  var traceExporter
+  var metricExporter
 
-  const metricExporter = new OTLPMetricExporter({
-    url: "http://apm-server:8200", // url is optional and can be omitted - default is http://localhost:55681/v1/traces
-    concurrencyLimit: 1000, // an optional limit on pending requests
-  });
+  if (process.env.OTEL_EXPORTER_OTLP_PROTOCOL == "http"){
+    traceExporter = new OTLPTraceExporterHTTP({
+      url: "http://apm-server:8200", // url is optional and can be omitted - default is http://localhost:55681/v1/traces
+      headers: {}, // an optional object containing custom headers to be sent with each request
+      concurrencyLimit: 1000, // an optional limit on pending requests
+    });
+
+    metricExporter = new OTLPMetricExporterHTTP({
+      url: "http://apm-server:8200", // url is optional and can be omitted - default is http://localhost:55681/v1/traces
+      concurrencyLimit: 1000, // an optional limit on pending requests
+    });
+  }
+  else {
+    traceExporter = new OTLPTraceExporterGRPC({
+      url: "http://apm-server:8200", // url is optional and can be omitted - default is http://localhost:55681/v1/traces
+      headers: {}, // an optional object containing custom headers to be sent with each request
+      concurrencyLimit: 1000, // an optional limit on pending requests
+    });
+
+    metricExporter = new OTLPMetricExporterGRPC({
+      url: "http://apm-server:8200", // url is optional and can be omitted - default is http://localhost:55681/v1/traces
+      concurrencyLimit: 1000, // an optional limit on pending requests
+    });
+  }
+
 
   const resource = new Resource({
     [ResourceAttributesSC.CONTAINER_ID]: execSync('basename $(cat /proc/1/cpuset)').toString()
